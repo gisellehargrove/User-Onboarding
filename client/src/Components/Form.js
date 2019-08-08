832ca59dcd12f2e5e0e4077c90636889d84e87a0
gisellehargrove <gisellehargrove@gmail.com>
2019-08-07 22:22:35 -0400

    display response from post request

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withFormik, Form, Field } from "formik";
 import * as Yup from 'yup';
 import axios from 'axios';
 
 
-function FormCreator({errors, touched, values}) {
+function FormCreator({errors, touched, values, status}) {
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    if(status) setUsers([...users, status])
+  }, [status])
+
+  console.log(users, 'formcreator users')
   return (
-    <Form>
+    <div>
+      <Form>
+        <div>
+          {touched.name && errors.name && <p>{errors.name}</p>}
+          <Field type="text" name="name" placeholder="Name" />
+        </div>
+        <div>
+          {touched.email && errors.email && <p>{errors.email}</p>}
+          <Field type="email" name="email" placeholder="Email" />
+        </div>
+        <div>
+          {touched.password && errors.password && <p>{errors.password}</p>}
+          <Field type="password" name="password" placeholder="Password" />
+        </div>
+        <div>
+          <label>
+            <Field type="checkbox" name="tos" checked={values.tos} />
+            Terms of Service
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </Form>
       <div>
-        {touched.name && errors.name && <p>{errors.name}</p>}
-        <Field type="text" name="name" placeholder="Name" />
+        {users.map((user, key) =>
+          <div key={key}>
+            {user.name} : {user.email}
+          </div>
+        )}
       </div>
-      <div>
-        {touched.email && errors.email && <p>{errors.email}</p>}
-        <Field type="email" name="email" placeholder="Email" />
-      </div>
-      <div>
-        {touched.password && errors.password && <p>{errors.password}</p>}
-        <Field type="password" name="password" placeholder="Password" />
-      </div>
-      <div>
-        <label>
-          <Field type="checkbox" name="tos" checked={values.tos} />
-          Terms of Service
-        </label>
-      </div>
-      <button type="submit">Submit</button>
-    </Form>
-  )
+    </div>
+  );
 }
 
 const FormikOnboardForm = withFormik({
@@ -51,11 +66,16 @@ const FormikOnboardForm = withFormik({
     name: Yup.string()
       .required()
   }),
-  handleSubmit(values) {
+  handleSubmit(values, { resetForm, setStatus }) {
+    console.log(values)
     axios.post('https://reqres.in/api/users', values)
       .then(response => {
         console.log(response);
-      })
+        setStatus(response.data);
+        resetForm();
+      }).catch(err => {
+        console.log(err);
+      });
   }
 })(FormCreator);
 
